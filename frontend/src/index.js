0987b122522def9cb9ce17eb6132df6eba8493d5
github username <d24dust@hotmail.com>
2021-05-11 15:52:18 -0400

    add: redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import './assets/index.css';
 import App from './containers/App';
-// import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
