5bc57615f64660d81b27c684f5cee95fffb94415
github username <d24dust@hotmail.com>
2021-05-12 16:29:29 -0400

    feat: NewUserForm

--- frontend/src/containers/Users.js
+++ frontend/src/containers/Users.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import { createUser } from '../actions/users';
+import NewUserForm from '../components/users/NewForm';
 
 class UsersContainer extends Component {
   render() {
     return (
       <div>
-        
+        <NewUserForm createUser={this.props.createUser}/>
       </div>
     );
   }
 }
 const mapStateToProps = state => (
   {
-
+    currentUser: state.users.currentUser
   }
 )
 const mapDispatchToProps = dispatch => (
   {
-
+    createUser: formData => dispatch(createUser(formData))
   }
 )
 export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
