bb06f3ac6fe8e2aadc1ad1dae943fe2222debe83
github username <d24dust@hotmail.com>
2021-05-13 14:58:24 -0400

    add: /profile route

--- frontend/src/components/AppRoutes.js
+++ frontend/src/components/AppRoutes.js
@@ -1,9 +1,10 @@
-
+import { Route } from "react-router-dom";
+import Users from "../containers/Users";
 
 export default function AppRoutes(props) {
   return (
     <>
-    
+    <Route exact path="/profile" component={Users}/>
     </>
   )
 }
