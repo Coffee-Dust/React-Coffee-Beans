28bccd212f60c75d6d8f0a7500a73ce059bac0e8
github username <d24dust@hotmail.com>
2021-05-13 17:39:40 -0400

    feat: handlesInputChange

--- frontend/src/components/users/LoginForm.js
+++ frontend/src/components/users/LoginForm.js
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
 function LoginForm() {
+  const [formData, setFormData] = useState({email: "", password: ""});
 
+  const handleChange = event=> {
+    setFormData({...formData, [event.target.name]: event.target.value})
+  }
+  const handleSubmit = event=> {
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} value={formData.email} />
+      <input type="password" onChange={handleChange} />
+    </form>
+  )
 }
