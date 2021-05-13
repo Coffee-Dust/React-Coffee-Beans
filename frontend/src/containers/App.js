1da7d4e9245bc6711409f4bd4ecef6131d9fd8ea
github username <d24dust@hotmail.com>
2021-05-13 15:04:38 -0400

    add: Navbar

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, } from 'react-router-dom';
 import '../assets/App.css';
 import AppRoutes from '../components/AppRoutes';
-
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-
+        <Navbar />
         <div className="App-wrapper">
           <AppRoutes />
         </div>
