fab50dd5b0d712fb804463480720a4def303cf90
github username <d24dust@hotmail.com>
2021-05-13 18:09:18 -0400

    feat: sessions#create action

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
 class SessionsController < ApplicationController
-  
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user).to_serialized_json
+    else
+      render json: {errors: ["Could not find account with that email/password!"]}, status: 403
+    end
+  end
 end
