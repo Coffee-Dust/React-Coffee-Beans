7acdab03eb05d2a46329231384adecc4c1fdf3df
github username <d24dust@hotmail.com>
2021-05-13 17:02:37 -0400

    feat: now serializes

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,9 +1,14 @@
 class UserSerializer
+
+  @@options = {
+    except: [:created_at, :updated_at]
+  }
+
   def initialize(user)
     @user = user
   end
 
   def to_serialized_json
-    
+    @user.to_json(@@options)
   end
 end
