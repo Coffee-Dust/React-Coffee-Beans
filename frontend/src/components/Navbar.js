99e770daa81234ef789e98f701c5a8febc3c23cb
github username <d24dust@hotmail.com>
2021-05-13 15:03:28 -0400

    add: /profile NavLink

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,9 @@
-
+import { NavLink } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <div className="App-navbar">
-
+      <NavLink to="/profile">Profile</NavLink>
     </div>
   )
 }
