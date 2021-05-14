e426d4295ced82e11a63fb639938617651379ff4
github username <d24dust@hotmail.com>
2021-05-14 12:04:10 -0400

    add: checkForUserSession function

--- frontend/src/helpers/user.js
+++ frontend/src/helpers/user.js
@@ -1,4 +1,6 @@
 import { store } from ".."
+import { addCurrentUser } from "../actions/users"
+import AjaxCall from "./ajax"
 
 export function isLoggedIn() {
   return !!currentUser()
@@ -6,4 +8,10 @@ export function isLoggedIn() {
 
 export function currentUser() {
   return store.getState().users.currentUser
+}
+
+export function checkForUserSession() {
+  new AjaxCall("sessions").getData().then(user=> {
+    store.dispatch(addCurrentUser(user))
+  })
 }
