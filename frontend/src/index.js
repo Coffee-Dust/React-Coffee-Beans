85ae4a65935da60faea8cc818254214e8b211404
github username <d24dust@hotmail.com>
2021-05-14 12:48:30 -0400

    change: exports store for non component use

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import App from './containers/App';
 import rootReducer from './reducers'
 import thunk from 'redux-thunk';
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
