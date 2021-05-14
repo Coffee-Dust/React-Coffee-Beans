727ea07584d0dff9568a865056ae4770b5dbdbcd
github username <d24dust@hotmail.com>
2021-05-14 12:43:34 -0400

    add: sessions index action

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
+
+  def index
+    if uid = session[:user_id]
+      render json: UserSerializer(User.find(uid)).to_serialized_json
+    else
+      render json: {errors: ["Session not found! Please login again."]}, status: 403
+    end
+  end
+
   def create
     user = User.find_by(email: params[:user][:email])
     if user.authenticate(params[:user][:password])
@@ -8,4 +17,5 @@ class SessionsController < ApplicationController
       render json: {errors: ["Could not find account with that email/password!"]}, status: 403
     end
   end
+
 end
