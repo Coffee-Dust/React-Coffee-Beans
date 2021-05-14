ef2f5da46931d1b679f392c974b09d6dc4312e87
github username <d24dust@hotmail.com>
2021-05-14 14:06:29 -0400

    add: NavLink for posts#index

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,6 +4,7 @@ import Paths from "../helpers/path";
 export default function Navbar(props) {
   return (
     <div className="App-navbar">
+      <NavLink to={Paths.posts}>Bean-Feed</NavLink>
       <NavLink to={Paths.profile}>Profile</NavLink>
     </div>
   )
