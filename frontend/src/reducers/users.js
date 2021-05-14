49b03b9ec1b66b894ee0eb32bc44a221ee2e1402
github username <d24dust@hotmail.com>
2021-05-14 12:43:10 -0400

    add: ADD_CURRENT_USER action handler

--- frontend/src/reducers/users.js
+++ frontend/src/reducers/users.js
@@ -1,6 +1,11 @@
 export default function usersReducer(state=false, action) {
   console.log(`usersReducer received ${action.type}`, action)
   switch (action.type) {
+    case "ADD_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.user
+      }
     default:
       return state
   }
