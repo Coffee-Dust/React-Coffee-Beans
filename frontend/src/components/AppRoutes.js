6c86c22320144827e97d9edcf483922168a0f583
github username <d24dust@hotmail.com>
2021-05-14 14:06:01 -0400

    refactor: imports&uses paths from PathsHelper

--- frontend/src/components/AppRoutes.js
+++ frontend/src/components/AppRoutes.js
@@ -1,12 +1,13 @@
 import { Route } from "react-router-dom";
 import Posts from "../containers/Posts";
 import Users from "../containers/Users";
+import Paths from "../helpers/path";
 
 export default function AppRoutes(props) {
   return (
     <>
-    <Route path="/beans" component={Posts}/>
-    <Route exact path="/profile" component={Users}/>
+    <Route path={Paths.posts} component={Posts}/>
+    <Route exact path={Paths.profile} component={Users}/>
     </>
   )
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router-dom";
+import Paths from "../helpers/path";
 
 export default function Navbar(props) {
   return (
     <div className="App-navbar">
-      <NavLink to="/profile">Profile</NavLink>
+      <NavLink to={Paths.profile}>Profile</NavLink>
     </div>
   )
 }
