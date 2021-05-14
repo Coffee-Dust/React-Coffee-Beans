035a280f2367779282ae3892a9e3202e0f9d9810
github username <d24dust@hotmail.com>
2021-05-14 14:03:53 -0400

    add: route for posts

--- frontend/src/components/AppRoutes.js
+++ frontend/src/components/AppRoutes.js
@@ -1,9 +1,11 @@
 import { Route } from "react-router-dom";
+import Posts from "../containers/Posts";
 import Users from "../containers/Users";
 
 export default function AppRoutes(props) {
   return (
     <>
+    <Route path="/beans" component={Posts}/>
     <Route exact path="/profile" component={Users}/>
     </>
   )
