a1e8e12e33f4005e1b175944e7d8d45958c5c080
github username <d24dust@hotmail.com>
2021-05-15 14:29:08 -0400

    add: posts reducer to combineReducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import postsReducer from "./posts";
 import usersReducer from "./users";
 
-export default combineReducers({users: usersReducer})
+export default combineReducers({users: usersReducer, posts: postsReducer})
