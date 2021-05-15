8c4486c3bfb31ef309875eed3973beccaa43ac89
github username <d24dust@hotmail.com>
2021-05-15 13:02:53 -0400

    update: constructor to throw error if missing id

--- frontend/src/models/post.js
+++ frontend/src/models/post.js
@@ -1,8 +1,14 @@
+import AjaxCall from "../helpers/ajax"
 class Post {
 
   constructor(data) {
-    this.content = data.content
-    this.user_id = data.user_id || null
+    if (data.id) {
+      this.id = data.id
+      this.content = data.content
+      this.user_id = data.user_id
+    } else {
+      throw new Error("Invalid Post Creation: Missing ID property")
+    }
   }
 
 
