b93bccb753960f7407a24b2d7b6c8fd82a876492
github username <d24dust@hotmail.com>
2021-05-15 14:34:03 -0400

    add: handleChange method

--- frontend/src/components/posts/Form.js
+++ frontend/src/components/posts/Form.js
@@ -4,6 +4,10 @@ function PostsForm(props) {
   
   const [postData, setPostData] = useState({content: props.post.content || ""});
 
+  const handleChange = event=> {
+    setPostData({...postData, content: event.target.value})
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleChange} name="content" value={postData.content}/>
