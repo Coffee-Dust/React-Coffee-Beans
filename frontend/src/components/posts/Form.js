88fe2675a7121a4f1ef87987a1eafb3de565d874
github username <d24dust@hotmail.com>
2021-05-15 14:33:24 -0400

    add: form render

--- frontend/src/components/posts/Form.js
+++ frontend/src/components/posts/Form.js
@@ -1,6 +1,20 @@
+import { useState } from "react"
+
 function PostsForm(props) {
+  
+  const [postData, setPostData] = useState({content: props.post.content || ""});
+
   return (
-    <form></form>
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} name="content" value={postData.content}/>
+      {
+        (!props.post) ?
+          <input type="file" accept="image/png, image/jpeg" name="image"/>
+        :
+          <input type="file" accept="image/png, image/jpeg" name="image" hidden="true"/>
+      }
+      <input type="submit" />
+    </form>
   )
 }
 
