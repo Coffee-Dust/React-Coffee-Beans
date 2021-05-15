22c6c1fc7e3071c281a3a6b363454e092a5c4ec3
github username <d24dust@hotmail.com>
2021-05-15 14:31:35 -0400

    add: state and actions to props

--- frontend/src/containers/Posts.js
+++ frontend/src/containers/Posts.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addPost } from '../actions/posts';
+import PostsForm from '../components/posts/Form';
 
 class PostsContainer extends Component {
   render() {
     return (
       <div>
-
+        <PostsForm addPost={this.props.actions.addPost} post={false} container={this.props}/>
       </div>
     );
   }
 }
 
 const mapStateToProps = state=> ({
-
+  currentUser: state.users.currentUser,
+  posts: state.posts.all
 })
 
 const mapDispatchToProps = dispatch=> ({
-
+  actions: {
+    addPost: post=> dispatch(addPost(post))
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
