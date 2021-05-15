1afb2b592fb0b1e5667685ebb9afeeed4a1edc36
github username <d24dust@hotmail.com>
2021-05-15 14:29:48 -0400

    create: ShowPost pres component

---
+++ frontend/src/components/posts/Show.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ShowPost extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Show;
+
