378536fb438468f8eca1b34ec48d991b98012bad
github username <d24dust@hotmail.com>
2021-05-19 19:18:35 -0400

    create: reactions container

---
+++ frontend/src/containers/Reactions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReactionsContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default ReactionsContainer;
