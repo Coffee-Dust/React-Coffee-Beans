6e91f160426fd968b25834c215b3867e6c323195
github username <d24dust@hotmail.com>
2021-05-19 19:54:42 -0400

    add: simple seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+Post.first.reactions.create(user_id: 1, reaction_type: "like")
+Post.first.reactions.create(user_id: 1, reaction_type: "sad")
+Post.first.reactions.create(user_id: 1, reaction_type: "angry")
+end
