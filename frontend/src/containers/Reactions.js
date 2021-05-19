f79fcfbd2438caa1800a9575b64ac89ebbe195b6
github username <d24dust@hotmail.com>
2021-05-19 19:54:27 -0400

    feat: generates buttons with count from parent

--- frontend/src/containers/Reactions.js
+++ frontend/src/containers/Reactions.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 
+const reactionTypes = {
+  "like": "👍",
+  "dislike": "👎",
+  "love": "💜",
+  "laugh": "🤣",
+  "angry": "😡",
+  "sad": "😢"
+}
+
 class ReactionsContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.parentType = this.props.parent.constructor.name
+    this.reactionsCount = this.props.parent.reactionsCount
+  }
+
+  generateReactionButtons() {
+    return Object.keys(reactionTypes).map(type=> {
+      let glyph = reactionTypes[type]
+      let count = this.reactionsCount[type]
+      return <Reaction glyph={glyph} count={count} handleClick={this.handleClick} />
+    })
+  }
+
+  handleClick(event) {
+
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <span className={`${this.parentType}s-reactions-bar`}>
+        {this.generateReactionButtons()}
+      </span>
     );
   }
+}
+
+function Reaction(props) {
+  return (
+    <button>{props.count}{props.glyph}</button>
+  )
 }
 
 export default ReactionsContainer;
