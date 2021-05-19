463bc342836a404893ff565db3303cf6a4649faa
github username <d24dust@hotmail.com>
2021-05-19 11:58:51 -0400

    add: user login feature

--- frontend/src/containers/Users.js
+++ frontend/src/containers/Users.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-import { createUser } from '../actions/users';
+import { createUser, loginUser } from '../actions/users';
+import LoginForm from '../components/users/LoginForm';
 import NewUserForm from '../components/users/NewForm';
 
 class UsersContainer extends Component {
   render() {
-    return (
-      <div>
-        <NewUserForm createUser={this.props.createUser}/>
-      </div>
-    );
+    if (this.props.currentUser) {
+      return (
+        <div>
+          <h1>Hello, {this.props.currentUser.name}!</h1>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <LoginForm loginUser={this.props.loginUser}/>
+          <NewUserForm createUser={this.props.createUser}/>
+        </div>
+      );
+    }
   }
 }
-const mapStateToProps = state => (
+
+const mapStateToProps = state=> (
   {
     currentUser: state.users.currentUser
   }
 )
-const mapDispatchToProps = dispatch => (
+const mapDispatchToProps = dispatch=> (
   {
-    createUser: formData => dispatch(createUser(formData))
+    createUser: formData => dispatch(createUser(formData)),
+    loginUser: formData=> dispatch(loginUser(formData))
   }
 )
 export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
