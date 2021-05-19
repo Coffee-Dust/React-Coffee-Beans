ded610fdd2a221ef5a9d82945a6c103a04b1c9a2
github username <d24dust@hotmail.com>
2021-05-19 12:24:32 -0400

    refactor: ShowPost to PostList

--- frontend/src/components/posts/List.js
+++ frontend/src/components/posts/List.js
@@ -3,17 +3,25 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as CommentIcon } from '../../assets/icons/comment.svg';
 import Paths from '../../helpers/path';
 
-function ShowPost({post}) {
+function PostList({posts}) {
   return (
-    <div className="Posts-show">
+    <ul className="Posts-feed">
+      {posts.map(post=> <Post key={`post${post.id}`} post={post} />)}
+    </ul>
+  )
+}
+
+function Post({post}) {
+  return (
+    <li className="Posts-show">
       <div className="Posts-show-user"><Link to={Paths.showUser(post.user)}>{post.user.name}</Link></div>
       <p>{post.content}</p>
       <div className="Posts-show-interactions-bar">
         <span className="Posts-show-comment-count">{<CommentIcon />} {post.commentCount}</span>
       </div>
-    </div>
+    </li>
   )
 }
 
-export default ShowPost;
+export default PostList;
 
