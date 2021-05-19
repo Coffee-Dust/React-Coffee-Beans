0c78506effad31bcbb384ba27f09af2665cd157d
github username <d24dust@hotmail.com>
2021-05-19 11:50:57 -0400

    feat: index action

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,3 +1,11 @@
 class PostsController < ApplicationController
 
+  def index
+    posts = Post.limit(params[:limit])
+    json = posts.map do |post| 
+      PostSerializer.new(post).to_serialized_json
+    end
+    render json: json
+  end
+
 end
