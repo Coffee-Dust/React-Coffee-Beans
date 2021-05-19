17fca8ab0c5f25639d0120c43396332b56111dc1
github username <d24dust@hotmail.com>
2021-05-19 11:51:56 -0400

    feat: create action

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     render json: json
   end
 
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      render json: PostSerializer.new(post).to_serialized_json
+    else
+      render json: {errors: post.errors}, status: 569
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: post_params[:user_id])
+  end
+
 end
