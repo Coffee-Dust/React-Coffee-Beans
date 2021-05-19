884fae2292e96042fcf1e313134ea72166d19a5a
github username <d24dust@hotmail.com>
2021-05-19 15:11:12 -0400

    add: appReducer to root index reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import appReducer from "./app";
 import postsReducer from "./posts";
 import usersReducer from "./users";
 
-export default combineReducers({users: usersReducer, posts: postsReducer})
+export default combineReducers({app: appReducer, users: usersReducer, posts: postsReducer})
