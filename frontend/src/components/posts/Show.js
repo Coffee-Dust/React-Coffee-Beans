55e68507cb01d04b6f313f10121346911c2ab89d
github username <d24dust@hotmail.com>
2021-05-19 11:57:56 -0400

    feat: displays data

--- frontend/src/components/posts/Show.js
+++ frontend/src/components/posts/Show.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CommentIcon } from '../../assets/icons/comment.svg';
+import Paths from '../../helpers/path';
 
-function ShowPost(props) {
+function ShowPost({post}) {
   return (
-    <div>
-
+    <div className="Posts-show">
+      <div className="Posts-show-user"><Link to={Paths.showUser(post.user)}>{post.user.name}</Link></div>
+      <p>{post.content}</p>
+      <div className="Posts-show-interactions-bar">
+        <span className="Posts-show-comment-count">{<CommentIcon />} {post.commentCount}</span>
+      </div>
     </div>
   )
 }
