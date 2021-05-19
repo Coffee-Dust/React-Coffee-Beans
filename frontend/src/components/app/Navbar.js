2e5820cc5546e6cc09c2d2cff16c8539318c132d
github username <d24dust@hotmail.com>
2021-05-19 15:09:08 -0400

    add: logo to navbar

--- frontend/src/components/app/Navbar.js
+++ frontend/src/components/app/Navbar.js
@@ -4,6 +4,7 @@ import Paths from "../../helpers/path";
 export default function Navbar(props) {
   return (
     <div className="App-navbar">
+      <NavLink to="/" className="App-logo"><span>Coffee-Beans</span></NavLink>
       <NavLink to={Paths.posts}>Bean-Feed</NavLink>
       <NavLink to={Paths.profile}>Profile</NavLink>
     </div>
