b54a167886ce39f48d401bbf2d4192e59e665b9a
github username <d24dust@hotmail.com>
2021-05-19 19:00:32 -0400

    refactor: move Post component into own file

--- frontend/src/components/posts/List.js
+++ frontend/src/components/posts/List.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { ReactComponent as CommentIcon } from '../../assets/icons/comment.svg';
-import Paths from '../../helpers/path';
+import Post from './Post'
 
 function PostList({posts}) {
   return (
     <div className="Posts-feed">
       {posts.map(post=> <Post key={`post${post.id}`} post={post} />)}
-    </div>
-  )
-}
-
-function Post({post}) {
-  return (
-    <div className="Posts-show">
-      <div className="Posts-show-user"><Link to={Paths.showUser(post.user)}>{post.user.name}</Link></div>
-      <div className="Posts-show-content"><p>{post.content}</p></div>
-      <div className="Posts-show-interactions-bar">
-        <span className="Posts-show-comment-count">{<CommentIcon />} {post.commentCount}</span>
-      </div>
     </div>
   )
 }
---
+++ frontend/src/components/posts/Post.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CommentIcon } from '../../assets/icons/comment.svg';
+import Paths from '../../helpers/path';
+
+function Post({ post }) {
+  return (
+    <div className="Posts-show">
+      <div className="Posts-show-user"><Link to={Paths.showUser(post.user)}>{post.user.name}</Link></div>
+      <span className="Posts-show-date">{post.date.toLocaleTimeString("en-us", { hour: '2-digit', minute: '2-digit' })} - {post.date.toDateString()}</span>
+      <div className="Posts-show-content"><p>{post.content}</p></div>
+      <div className="Posts-show-interactions-bar">
+        <span className="Posts-show-comment-count">{<CommentIcon />} {post.commentCount}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Post;
