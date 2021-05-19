c88cbe5d3c52418bfa836509b390e574f022a1a8
github username <d24dust@hotmail.com>
2021-05-19 11:55:34 -0400

    add: fetchPosts action

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,3 +1,6 @@
+import AjaxCall from "../helpers/ajax" 
+import Post from "../models/post"
+
 export function addPost(post) {
   if (post.constructor.name === "Post") {
     return {
@@ -8,3 +11,15 @@ export function addPost(post) {
     throw new Error(`InvalidItemAddedToStoreError: tried to add ${post} when it was not of type "Post"`)
   }
 }
+
+export function fetchPosts(limit=25) {
+  return dispatch=> {
+    dispatch({type: "FETCH_POSTS"})
+    new AjaxCall(`/beans?limit=${limit}`).getData().then(postsData=> {
+      dispatch({type: "ADD_POSTS", posts: postsData.map(data=> new Post(data))})
+    })
+  }
+}
+
+// Action creators can be impure.
+// var addChat = ({id = "JKLDSHFJKSDHFJKHSDJKHF", msg = '', user = 'Anonymous', timeStamp = Date.now()} = {idk:"XD"}) => ({ type: "ADD_CHAT", payload: { id, msg, user, timeStamp }});
