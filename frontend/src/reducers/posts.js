b8d0ae2f1040a827e9faa959d2ac838cb6d0e4e8
github username <d24dust@hotmail.com>
2021-05-19 19:57:27 -0400

    change: ADD_POST adds new post first on the array

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -2,7 +2,7 @@ export default function postsReducer(state = {all:[], status:{}}, action) {
   console.log(`postsReducer received ${action.type}`, action)
   switch (action.type) {
     case "ADD_POST":
-      return {...state, all: [...state.all, action.post]}
+      return {...state, all: [action.post, ...state.all]}
     case "FETCH_POSTS":
       return {...state, status: {...state.status, pendingFetch: true}}
     case "ADD_POSTS":
