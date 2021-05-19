5d942d7bea112d3b98ac64127872f91e088a25e8
github username <d24dust@hotmail.com>
2021-05-19 18:08:56 -0400

    change: html structure

--- frontend/src/components/posts/List.js
+++ frontend/src/components/posts/List.js
@@ -5,21 +5,21 @@ import Paths from '../../helpers/path';
 
 function PostList({posts}) {
   return (
-    <ul className="Posts-feed">
+    <div className="Posts-feed">
       {posts.map(post=> <Post key={`post${post.id}`} post={post} />)}
-    </ul>
+    </div>
   )
 }
 
 function Post({post}) {
   return (
-    <li className="Posts-show">
+    <div className="Posts-show">
       <div className="Posts-show-user"><Link to={Paths.showUser(post.user)}>{post.user.name}</Link></div>
-      <p>{post.content}</p>
+      <div className="Posts-show-content"><p>{post.content}</p></div>
       <div className="Posts-show-interactions-bar">
         <span className="Posts-show-comment-count">{<CommentIcon />} {post.commentCount}</span>
       </div>
-    </li>
+    </div>
   )
 }
 
