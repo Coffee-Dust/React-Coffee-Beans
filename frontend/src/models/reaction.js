3b9f66344d8921cd9199a2c8eec12e633699e8cd
github username <d24dust@hotmail.com>
2021-05-20 13:33:21 -0400

    add: constructor & static typeGlyphs

--- frontend/src/models/reaction.js
+++ frontend/src/models/reaction.js
@@ -1,3 +1,24 @@
 class Reaction {
-  
+
+  static get typeGlyphs() {
+    return {
+      "like": "👍",
+      "dislike": "👎",
+      "love": "💜",
+      "laugh": "🤣",
+      "angry": "😡",
+      "sad": "😢"
+    }
+  }
+
+  constructor(type, parent, currentUser) {
+    this.type = type
+    this.glyph = this.constructor.typeGlyphs[type]
+    this.reactableID = parent.id
+    this.reactableType = parent.constructor.name
+    this.currentUser = currentUser
+  }
+
 }
+
+export default Reaction;
