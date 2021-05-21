22fb57290edf9589a3a8793e81c9c75230413b07
github username <d24dust@hotmail.com>
2021-05-21 15:19:19 -0400

    feat: submitClick method

--- frontend/src/models/reaction.js
+++ frontend/src/models/reaction.js
@@ -1,3 +1,6 @@
+import AjaxCall from "../helpers/ajax"
+import Paths from "../helpers/path"
+
 class Reaction {
 
   static get typeGlyphs() {
@@ -17,6 +20,16 @@ class Reaction {
     this.reactableID = parent.id
     this.reactableType = parent.constructor.name
     this.currentUser = currentUser
+  }
+
+  submitClick(successCallback) {
+    const postableData = {
+      reaction_type: this.type,
+      reactable_id: this.reactableID,
+      reactable_type: this.reactableType,
+      user_id: this.currentUser.id
+    }
+    new AjaxCall(Paths.reactions).postData(postableData).then(newReactionsCount=> successCallback(newReactionsCount))
   }
 
 }
