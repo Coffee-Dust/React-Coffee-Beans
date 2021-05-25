c77f8282caedc52cadd0385f6479e49079f697cb
github username <d24dust@hotmail.com>
2021-05-25 18:26:49 -0400

    add: POST reactions route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   resources :posts, path: "beans"
+  
+  post :reactions, to: 'reactions#react_or_remove'
 
   resources :sessions, only: [:create, :destroy, :index]
 end
