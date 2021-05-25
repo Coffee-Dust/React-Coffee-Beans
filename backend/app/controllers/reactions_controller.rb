075421c440c814ff0004529c57cf5f6899c46224
github username <d24dust@hotmail.com>
2021-05-25 18:19:51 -0400

    add: strong params

--- backend/app/controllers/reactions_controller.rb
+++ backend/app/controllers/reactions_controller.rb
@@ -1,9 +1,15 @@
 class ReactionsController < ApplicationController
   def react_or_remove
 
+    @reaction = Reaction.find_or_build_reaction_with(reaction_params, current_user)
+
   end
 
 
   private
+
+  def reaction_params
+    params.require(:reaction).permit(:reaction_type, :user_id, :reactable_id, :reactable_type)
+  end
 
 end
