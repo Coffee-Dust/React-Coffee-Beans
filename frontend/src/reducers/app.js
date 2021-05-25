631a364439cd6e61b88ae8a4caef3e76d563f49f
github username <d24dust@hotmail.com>
2021-05-25 18:39:58 -0400

    change: console log

--- frontend/src/reducers/app.js
+++ frontend/src/reducers/app.js
@@ -1,5 +1,5 @@
 export default function appReducer(state = {currentPopup: false}, action) {
-  console.log(`appReducer received ${action.type}`, action)
+  console.log(`Reducers received ${action.type}`, action)
   switch (action.type) {
     case "DISPLAY_POPUP":
       return { ...state, currentPopup: {content: action.payload.content, onClose: action.payload.onClose} }
