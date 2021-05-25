c247a2bef76328d4f0f8bd79ab6bd09980da92e4
github username <d24dust@hotmail.com>
2021-05-25 18:43:54 -0400

    feat: displays content

--- frontend/src/components/posts/Fullscreen.js
+++ frontend/src/components/posts/Fullscreen.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CommentIcon } from '../../assets/icons/comment.svg';
+import ReactionsContainer from '../../containers/Reactions';
+import Paths from '../../helpers/path';
 
 class FullscreenPost extends Component {
-  constructor(props) {
-    super()
-    this.container = props.container
-    this.post = props.post
-  }
+
   render() {
     return (
-      <div>
-        <button onClick={e=> this.container.closeFocus(this.post)}>X</button>
-        <p>{this.post.content}</p>
+      <div className="Posts-show-fullscreen">
+        <div className="Posts-show">
+        <div className="Posts-show-user"><Link to={Paths.showUser(this.props.post.user)}>{this.props.post.user.name}</Link></div>
+        <span className="Posts-show-date">{this.props.post.date.toLocaleTimeString("en-us", { hour: '2-digit', minute: '2-digit' })} - {this.props.post.date.toDateString()}</span>
+        <div className="Posts-show-content"><p>{this.props.post.content}</p></div>
+        <div className="Posts-show-interactions-bar">
+          <ReactionsContainer parent={this.props.post} />
+          <span className="Posts-show-comment-count"> {<CommentIcon />} {this.props.post.commentCount}</span>
+        </div>
+        </div>
       </div>
     );
   }
 }
 
-export default FullscreenPost;
+export default connect(state=> ({allPosts: state.posts.all}))(FullscreenPost);
