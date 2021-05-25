577f00901bffb148c8eb2afc59be8f6473283d96
github username <d24dust@hotmail.com>
2021-05-25 18:20:10 -0400

    add: current_user method

--- backend/app/controllers/reactions_controller.rb
+++ backend/app/controllers/reactions_controller.rb
@@ -12,4 +12,8 @@ class ReactionsController < ApplicationController
     params.require(:reaction).permit(:reaction_type, :user_id, :reactable_id, :reactable_type)
   end
 
+  def current_user
+    @current_user ||= User.find_by(id: reaction_params[:user_id])
+  end
+
 end
