1204956842b6a43052644e14cfd447ad8ef33b1e
github username <d24dust@hotmail.com>
2021-05-26 17:23:03 -0400

    feat: index action

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,3 +1,8 @@
-class CommentsController
+class CommentsController < ApplicationController
   
+  def index
+    comments = Post.find_by(id: params[:post_id]).comments
+    render json: CommentSerializer.new(comments).to_serialized_json
+  end
+
 end
