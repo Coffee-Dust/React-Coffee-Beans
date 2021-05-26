9038b6041cf52af985da7392018482b4bbce31e9
github username <d24dust@hotmail.com>
2021-05-26 17:24:01 -0400

    add: nested comment routes for posts

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
-  resources :posts, path: "beans"
+  resources :posts, path: "beans" do
+    resources :comments, except: [:show, :edit, :new]
+  end
   
   post :reactions, to: 'reactions#react_or_remove'
 
