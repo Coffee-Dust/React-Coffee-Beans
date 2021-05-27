8c6072c17fbf979bead0e4402cf747a5500c3809
github username <d24dust@hotmail.com>
2021-05-26 20:29:08 -0400

    add: constructor method for state and props init

--- frontend/src/containers/Comments.js
+++ frontend/src/containers/Comments.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class CommentsContainer extends Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      comments: []
+    }
+    this.post = props.post
+  }
+
   render() {
     return (
       <div>
