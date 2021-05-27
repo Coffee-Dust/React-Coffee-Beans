ec25630f475ea5e57dafbdc3ca7feb147cd3014c
github username <d24dust@hotmail.com>
2021-05-27 13:17:53 -0400

    add: controlled input for comment.content

--- frontend/src/components/comments/NewForm.js
+++ frontend/src/components/comments/NewForm.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NewCommentForm(props) {
+
+  const [content, setContent] = useState("");
 
   const handleSubmit = event=> {
     event.preventDefault();
@@ -9,7 +11,7 @@ function NewCommentForm(props) {
   return (
     <div className="Comments-new-form">
       <form onSubmit={handleSubmit}>
-
+        <input type="text" name="content" value={content} onChange={e=> setContent(e.target.value)}/>
       </form>
     </div>
   );
