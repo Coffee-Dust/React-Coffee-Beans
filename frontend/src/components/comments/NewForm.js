9c4271acc3c8d0883e68703beac4f8d14bfcdfe4
github username <d24dust@hotmail.com>
2021-05-27 13:16:25 -0400

    add: form render with submitHandler method

--- frontend/src/components/comments/NewForm.js
+++ frontend/src/components/comments/NewForm.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function NewCommentForm(props) {
+
+  const handleSubmit = event=> {
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      
+    <div className="Comments-new-form">
+      <form onSubmit={handleSubmit}>
+
+      </form>
     </div>
   );
 }
