5f29696bec824bd0c3881b070376ff59d3db169c
github username <d24dust@hotmail.com>
2021-05-27 14:04:32 -0400

    feat: Comment.create static method

--- frontend/src/models/comment.js
+++ frontend/src/models/comment.js
@@ -1,9 +1,22 @@
+import AjaxCall from "../helpers/ajax";
+import Paths from "../helpers/path";
+
 class Comment {
   
   constructor(data) {
     this.id = data.id
     this.user = data.user
     this.content = data.content
+  }
+
+  static create(data, successCallback) {
+    new AjaxCall(Paths.showPostComments({id: data.post_id})).postData({comment: data}).then(commentData=> {
+      if (commentData.id) {
+        successCallback(new this(commentData))
+      } else {
+        alert("Could not submit/save comment.")
+      }
+    })
   }
 
 }
