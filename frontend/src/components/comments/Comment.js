328a01759154260b28066d7b6d166caea8c8077e
github username <d24dust@hotmail.com>
2021-05-28 13:31:08 -0400

    change: Comment's user to Link

--- frontend/src/components/comments/Comment.js
+++ frontend/src/components/comments/Comment.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Paths from '../../helpers/path';
 
 function Comment({content, user}) {
   return (
     <div className="Comments-show">
-      <div className="Comments-show-user">{user.name}</div>
+      <div className="Comments-show-user"><Link to={Paths.showUser(user)}>{user.name}</Link></div>
       <p className="Comments-show-content">{content}</p>
     </div>
   );
