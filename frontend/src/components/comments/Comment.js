8ed7f191a3d0c3be039565180985acbf4df1025c
github username <d24dust@hotmail.com>
2021-05-29 13:05:24 -0400

    add: comment's date using Moment.js

--- frontend/src/components/comments/Comment.js
+++ frontend/src/components/comments/Comment.js
@@ -1,11 +1,20 @@
+import moment from 'moment';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Paths from '../../helpers/path';
 
-function Comment({content, user}) {
+function Comment({content, user, date}) {
+  const displayElapsedTime = ()=> {
+    const difference = new Date(Date.now() - date)
+    return moment(date).fromNow()
+  }
+
   return (
     <div className="Comments-show">
       <div className="Comments-show-user"><Link to={Paths.showUser(user)}>{user.name}</Link></div>
+      <div className="Comments-show-date">
+        {displayElapsedTime()}
+      </div>
       <p className="Comments-show-content">{content}</p>
     </div>
   );
