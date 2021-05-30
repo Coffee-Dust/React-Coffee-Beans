cc54aab0381b1bec92ebd7ac4ebc311483adc645
github username <d24dust@hotmail.com>
2021-05-30 14:08:50 -0400

    add: show route for users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create]
+  
+  resources :users, only: [:create, :show]
+
   resources :posts, path: "beans" do
     resources :comments, except: [:show, :edit, :new]
   end
