2472a112fa2eb9f8e11c3551e602d66cd81f6866
github username <d24dust@hotmail.com>
2021-05-30 13:51:06 -0400

    add: constructor method

--- frontend/src/components/users/Show.js
+++ frontend/src/components/users/Show.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class ShowUser extends Component {
+
+  constructor(props) {
+    super(props)
+    this.user = props.user
+    this.state = {
+      posts: []
+    }
+  }
+
   render() {
     return (
       <div>
