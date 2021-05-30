86e4c5f651965f71f369a133c0f6e11b2f319047
github username <d24dust@hotmail.com>
2021-05-30 18:05:34 -0400

    add: showUser path route

--- frontend/src/components/app/AppRoutes.js
+++ frontend/src/components/app/AppRoutes.js
@@ -9,6 +9,7 @@ export default function AppRoutes(props) {
     <Route exact path={Paths.posts} component={Posts}/>
     <Route exact path={Paths.showPost({ id: ':id' })} component={Posts} />
     <Route exact path={Paths.profile} component={Users}/>
+    <Route exact path={Paths.showUser({id: ':id'})} component={Users} />
     </>
   )
 }
