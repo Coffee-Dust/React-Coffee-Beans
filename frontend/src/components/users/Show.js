9e132ad6a8aa486f3b4c50cdeb62f4fa229bbfd2
github username <d24dust@hotmail.com>
2021-05-30 14:13:26 -0400

    temp: render PostList component

--- frontend/src/components/users/Show.js
+++ frontend/src/components/users/Show.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PostList from '../posts/List';
 
 class ShowUser extends Component {
 
@@ -17,7 +18,7 @@ class ShowUser extends Component {
   render() {
     return (
       <div>
-        
+        <PostList posts={this.state.posts} />
       </div>
     );
   }
