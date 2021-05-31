514e0eccc0e030d586b404b3baf6ba50776c831c
github username <d24dust@hotmail.com>
2021-05-31 15:56:52 -0400

    add: PENDING_USER_FETCH action

--- frontend/src/reducers/users.js
+++ frontend/src/reducers/users.js
@@ -1,8 +1,14 @@
 export default function usersReducer(state=false, action) {
   switch (action.type) {
+    case "PENDING_USER_FETCH":
+      return {
+        ...state,
+        pendingFetch: true
+      }
     case "ADD_CURRENT_USER":
       return {
         ...state,
+        pendingFetch: false,
         currentUser: action.user
       }
     default:
