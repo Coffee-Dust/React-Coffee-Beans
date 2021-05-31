3bd5ce4eadcbeec22a3c03cb818c56e0dac56652
github username <d24dust@hotmail.com>
2021-05-31 16:27:00 -0400

    refactor: LoginForm/NewUserForm to Signup component

--- frontend/src/containers/Users.js
+++ frontend/src/containers/Users.js
@@ -1,18 +1,15 @@
 import React, { PureComponent } from 'react';
 import {connect} from 'react-redux'
-import { displayPopup, closePopup } from '../actions/app';
-import { createUser, loginUser } from '../actions/users';
-import LoginForm from '../components/users/LoginForm';
-import NewUserForm from '../components/users/NewForm';
+import { displayPopup } from '../actions/app';
 import ShowUser from '../components/users/Show';
+import Signup from '../components/users/Signup';
 
 class UsersContainer extends PureComponent {
   
   componentDidMount() {
     if (!this.props.currentUser) {
       this.props.app.displayPopup([
-        <LoginForm loginUser={this.props.loginUser}/>,
-        <NewUserForm createUser={this.props.createUser}/>
+        <Signup />
       ])
     }
   }
@@ -41,11 +38,7 @@ const mapStateToProps = state=> (
 )
 const mapDispatchToProps = dispatch=> (
   {
-    createUser: formData=> dispatch(createUser(formData)),
-    loginUser: formData=> dispatch(loginUser(formData)),
-    app: {
-      displayPopup: content=> dispatch(displayPopup(content))
-    }
+    displayPopup: content=> dispatch(displayPopup(content))
   }
 )
 export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
