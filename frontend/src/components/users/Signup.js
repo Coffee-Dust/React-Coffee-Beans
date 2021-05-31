1b5aaf52a3abc49d96d0853dbf16f29c7ba48476
github username <d24dust@hotmail.com>
2021-05-31 18:22:44 -0400

    add: titles for forms

--- frontend/src/components/users/Signup.js
+++ frontend/src/components/users/Signup.js
@@ -5,7 +5,9 @@ import NewUserForm from './NewForm';
 function Signup() {
   return (
     <div className="Users-signup-popup">
+      <h2>Enter Your Login Info</h2>
       <LoginForm />,
+      <h2>Or Make a New Account</h2>
       <NewUserForm />
     </div>
   );
