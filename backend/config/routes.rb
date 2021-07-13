1cb422db187189776897af32ce5aeed14417e021
github username <d24dust@hotmail.com>
2021-07-13 16:23:15 -0400

    add: root route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'static#index'
+
   resources :users, only: [:create, :show] do
     resources :posts, path: "beans", only: [:index]
   end
